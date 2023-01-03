a54d2f4d5f94b087c87b13b6bd5801382d5b7da1
Daniil Siamenka <maloigb@gmail.com>
2023-01-03 21:10:10 +0300

    next

--- gendiff.js
+++ gendiff.js
@@ -4,8 +4,10 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.8.0'); 
-
+    .version('0.8.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format')
+    
 program.parse();
     
 
